8da6362f1f0bb6c965ce2639eb5174450a04e927
zavox <andras.kesik@gmail.com>
2016-04-25 11:42:27 +0200

    mondaycollection t1 t2 t3

---
+++ MondayCollections/Task2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MondayCollections
+{
+    class Task2
+    {
+        public static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            Console.WriteLine("Write integers separated with commas");
+            string[] input = Console.ReadLine().Split(',');
+            foreach (var elem in input)
+            {
+                try
+                {
+                    stack.Push(int.Parse(elem));
+                }
+                catch
+                {
+                    Console.WriteLine("found a not-integer in the input");
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine($"From Stack: {stack.Pop()}");
+            }
+
+        }
+    }
+}
---
+++ MondayCollections/Task3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MondayCollections
+{
+    class Task3
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict["One"] = 1;
+            dict["Two"] = 2;
+            dict["Three"] = 3;
+            dict["Four"] = 4;
+            dict["Zero"] = 0;
+            dict["Five"] = 5;
+            dict["Six"] = 6;
+            dict["Seven"] = 7;
+            dict["Eight"] = 8;
+            dict["Nine"] = 9;
+
+            string ourNumber = "three";
+
+            if (dict.ContainsKey(ourNumber))
+            {
+                Console.WriteLine(dict[ourNumber]);
+            }
+
+          
+              
+        }
+    }
+}
